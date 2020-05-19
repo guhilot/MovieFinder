19cf9191fc4070abfc11fcf1f37d839e318ac4de
vikramguhilot <guhilot@pdx.edu>
2020-05-19 15:49:22 -0700

    Updates

---
+++ precache-manifest.d065f3b682ceb40ec622f4a8633ee067.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f1a6230f16946af9538d57fd1f5449",
+    "url": "/MovieFinder/index.html"
+  },
+  {
+    "revision": "6f8d797ef2c54e7bc390",
+    "url": "/MovieFinder/static/css/main.66ce4e14.chunk.css"
+  },
+  {
+    "revision": "cb0b996786fee802142b",
+    "url": "/MovieFinder/static/js/2.5664acc8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MovieFinder/static/js/2.5664acc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f8d797ef2c54e7bc390",
+    "url": "/MovieFinder/static/js/main.5af07da8.chunk.js"
+  },
+  {
+    "revision": "5a6bdb0135e0d8dc7371",
+    "url": "/MovieFinder/static/js/runtime-main.c07a0a86.js"
+  }
+]);
